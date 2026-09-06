90a26f3207eac3b0a3c9c2ff6ff3a6a330c021cb
glassgrass-art <zqy838244033@gmail.com>
2026-09-05 20:32:53 -0500

    Change import of CollectionGrid to named import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import CollectionGrid from './components/CollectionGrid';
+import { CollectionGrid } from './components/CollectionGrid';
 import { TradePlaza } from './components/TradePlaza';
 import { getUserTradeStatus } from './utils/tradeMatcher';
 
